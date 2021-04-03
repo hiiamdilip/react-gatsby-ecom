73920dc0e176387793fee10a023ac57542371817
Dilip Kumar <dilip35kumar@gmail.com>
2021-04-03 14:49:29 +0530

    web address updated

---
+++ src/pages/contact.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { graphql } from 'gatsby'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from '../components/Reusable/HeroSection'
+import InfoBlock from '../components/Reusable/InfoBlock'
+import DualInfoBlock from '../components/Reusable/DualInfoBlock'
+import TeamPhotoSection from '../components/About/TeamPhotoSection'
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+    img={data.img.childImageSharp.fluid}
+    title="About LearnCodeOnline"
+    subtitle=""
+    heroclass="about-background"
+    />
+    <DualInfoBlock 
+    heading="A message from CEO"
+    img="https://images.pexels.com/photos/2756844/pexels-photo-2756844.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    />
+    <InfoBlock heading="About Vison"/>
+    <TeamPhotoSection/>
+  </Layout>
+)
+
+export const query = graphql`
+{
+  img: file(relativePath: { eq: "about.png" }) {
+    childImageSharp {
+      fluid {
+      ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
+
+export default AboutPage
